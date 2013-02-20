360945b755cf608285e3df4f24d54a6363840780
Rowena <rluk@dimagi.com>
2013-02-19 23:24:04 -0500

    add filtering to location admin ui

--- lib/rapidsms/contrib/locations/admin.py
+++ lib/rapidsms/contrib/locations/admin.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from models import Location, LocationType, Point
 
 class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "type")
+    list_filter = ('type', 'is_active')
     model = Location
 
 class LocationTypeAdmin(admin.ModelAdmin):
